5983d1a1bf8c0aedf6be19360a716172b64976c3
Milena Guerra <milenaoguerra5@gmail.com>
2026-01-14 18:00:09 -0300

    Implementa Tema e relacionamento com Postagem

--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,59 +1,75 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { DeleteResult, ILike, Repository } from "typeorm";
 import { Postagem } from "../entities/postagem.entity";
-import { ILike, Repository } from "typeorm";
-import { DeleteResult } from "typeorm/browser";
+import { TemaService } from "../../tema/services/tema.service";
 
 @Injectable()
 export class PostagemService {
     constructor(
-        @InjectRepository(Postagem) 
-        private postagemRepository: Repository<Postagem>
+        @InjectRepository(Postagem)
+        private postagemRepository: Repository<Postagem>,
+        private temaService: TemaService
     ) { }
 
     async findAll(): Promise<Postagem[]> {
-        return this.postagemRepository.find();
+        return await this.postagemRepository.find({
+            relations:{
+                tema: true
+            }
+        });
     }
 
-    // Find postagem por id
     async findById(id: number): Promise<Postagem> {
 
-        const postagem = await this.postagemRepository.findOne({
+        let postagem = await this.postagemRepository.findOne({
             where: {
                 id
+            },
+            relations:{
+                tema: true
             }
         });
 
-        if (!postagem) {
-            throw new HttpException('Postagem não encontrada',  HttpStatus.NOT_FOUND);
-        }
-            return postagem;
+        if (!postagem)
+            throw new HttpException('Postagem não encontrada!', HttpStatus.NOT_FOUND);
+
+        return postagem;
     }
 
-    // Find postagens por titulo
     async findAllByTitulo(titulo: string): Promise<Postagem[]> {
         return await this.postagemRepository.find({
-            where: {
+            where:{
                 titulo: ILike(`%${titulo}%`)
+            },
+            relations:{
+                tema: true
             }
-        });
+        })
     }
 
-    // Create a nova postagem
     async create(postagem: Postagem): Promise<Postagem> {
+
+        await this.temaService.findById(postagem.tema.id);
+        
         return await this.postagemRepository.save(postagem);
     }
 
-    // Update a postagem
     async update(postagem: Postagem): Promise<Postagem> {
+        
         await this.findById(postagem.id);
+
+        await this.temaService.findById(postagem.tema.id);
+        
         return await this.postagemRepository.save(postagem);
     }
 
-    // Delete a postagem
     async delete(id: number): Promise<DeleteResult> {
+        
         await this.findById(id);
+
         return await this.postagemRepository.delete(id);
-    }   
+
+    }
 
 }
