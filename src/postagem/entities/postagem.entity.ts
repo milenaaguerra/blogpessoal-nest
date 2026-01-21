9d3138324e25ff873af4a4506771dff4193414ea
Milena Guerra <milenaoguerra5@gmail.com>
2026-01-20 21:55:19 -0300

    finaliza recurso usuario e autenticacao

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -1,6 +1,7 @@
 import { IsNotEmpty } from "class-validator";
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { Tema } from "../../tema/entities/tema.entity";
+import { Usuario } from "../../usuario/entities/usuario.entity";
 
 @Entity({ name: 'tb_postagem' })
 export class Postagem {
@@ -23,5 +24,11 @@ export class Postagem {
         onDelete: 'CASCADE'
     })
     tema: Tema;
+
+    @ManyToOne(() => Usuario, (usuario) => usuario.postagem, {
+        onDelete: 'CASCADE'
+    })
+    usuario: Usuario;
     
-}
+    }
+    
--- src/tema/tema.module.ts
+++ src/tema/tema.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Tema } from "./entities/tema.entity";
-import { TemaController } from "./controllers/rema.controller";
+import { TemaController } from "./controllers/tema.controller";
 import { TemaService } from "./services/tema.service";
 
 @Module({
